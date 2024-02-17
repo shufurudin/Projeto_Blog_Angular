4f5dd2d52db6d9e281a1447c714f09181a812758
shufurudin <156028133+shufurudin@users.noreply.github.com>
2024-02-17 00:53:27 -0300

    Tornando os componentes reutilizáveis

--- angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular-blog/src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -10,7 +10,10 @@ export class SmallCardComponent implements OnInit {
   stock_img_size_x: number = 200
   stock_img_size_y: number = 100
   width: string = this.stock_img_size_x+'px'
-  img_src: string = "https://miro.medium.com/v2/resize:fit:1200/1*udvSMrSVGOgD4fxjMJHbOw.jpeg" || `https://placehold.co/${this.stock_img_size_x}x${this.stock_img_size_y}`;
+  @Input()
+  img_src: string = "" || `https://placehold.co/${this.stock_img_size_x}x${this.stock_img_size_y}`;
+  @Input()
+  card_title: string = ""
 
   constructor() { }
 
